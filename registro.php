4ce9f984b49777e38ffb77ece403c9c80c71b0c8
Gabriel Serrano <gabo09link@gmail.com>
2022-06-07 23:01:44 -0500

    Agregando inputs y favicon

---
+++ registro.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./img/favicon.png">
+    <title>Registro</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" placeholder="Nombre" required>
+        <input type="text" placeholder="Apellidos" required>
+        <input type="number" name="numTelefono" id="numTelefono" required>
+        <input type="text" placeholder="Dirección" required>
+        <input type="email" name="correo" id="correo" required>
+        <input type="password" name="password" id="password" required>
+        <input type="password" name="passwordConfirm" id="passwordConfirm" required>
+        <input type="submit" value="Registrarme">
+        
+    </form>
+    
+</body>
+</html>
