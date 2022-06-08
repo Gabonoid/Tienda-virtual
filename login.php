5ff921b0e0ed11fe88aef4c35a2a1116313f3fc0
Gabriel Serrano <gabo09link@gmail.com>
2022-06-07 22:56:28 -0500

    Agregado inputs

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <form method="post">
+        <input type="email" name="email" id="email" placeholder="correo@dominio.com" required>
+        <input type="password" name="password" id="password" required>
+        <input type="submit" value="Ingresar">
+    </form>
+
+    <input type="button" value="¿Olvidaste tu contraseña?">
+    <input type="button" value="Registrarme">
+
+</body>
+
+</html>
