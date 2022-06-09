55ef31f12231b010de8c04376a64f7e1acb5ee77
Gabriel Serrano <gabo09link@gmail.com>
2022-06-09 05:18:21 -0500

    estructura del nav

--- includes/nav.php
+++ includes/nav.php
@@ -1,21 +1,24 @@
 <?php
+
+$nombre = (isset($_SESSION['usuario'])) ? $_SESSION['usuario'] : "Inicia Sesion";
+
 $navMenu = '
 <nav>
     <div class="nav_left">
-        <a href="#"><img src="./img/logo_letter.png" alt="" /></a>
+        <a href="./index.php"><img src="./img/logo_letter.png" alt="" /></a>
         <input type="text" class="input_entrada" placeholder="Buscar..." />
         <button type="submit" class="btn_search" />
     </div>
 
     <div class="nav_right">
         <div class="nav_link">
-            <a href="#"><img src="./img/profile_icon.png" alt="" />Sesion</a>
+            <a href="./perfil.php"><img src="./img/profile_icon.png" alt="" />' . $nombre . '</a>
         </div>
         <div class="nav_link">
-            <a href="#"><img src="./img/fav_icon.png" alt="" />Favoritos</a>
+            <a href="./favoritos.php.php"><img src="./img/fav_icon.png" alt="" />Favoritos</a>
         </div>
         <div class="nav_link">
-            <a href="#"><img src="./img/cart_icon.png" alt="" />Carrito()</a>
+            <a href="./carrito.php"><img src="./img/cart_icon.png" alt="" />Carrito()</a>
         </div>
     </div>
 </nav>
