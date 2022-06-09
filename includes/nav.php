0e452e63469f2ebebbaa73938131a144a124261c
Gabriel Serrano <gabo09link@gmail.com>
2022-06-09 04:24:31 -0500

    guardando nav en una variable

---
+++ includes/nav.php
@@ -0,0 +1,23 @@
+<?php
+$navMenu = '
+<nav>
+    <div class="nav_left">
+        <a href="#"><img src="./img/logo_letter.png" alt="" /></a>
+        <input type="text" class="input_entrada" placeholder="Buscar..." />
+        <button type="submit" class="btn_search" />
+    </div>
+
+    <div class="nav_right">
+        <div class="nav_link">
+            <a href="#"><img src="./img/profile_icon.png" alt="" />Sesion</a>
+        </div>
+        <div class="nav_link">
+            <a href="#"><img src="./img/fav_icon.png" alt="" />Favoritos</a>
+        </div>
+        <div class="nav_link">
+            <a href="#"><img src="./img/cart_icon.png" alt="" />Carrito()</a>
+        </div>
+    </div>
+</nav>
+
+';
